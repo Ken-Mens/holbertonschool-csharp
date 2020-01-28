259e981455c42e55ee691251266a8af2fac943d2
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-28 12:40:18 -0800

    add print hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 99; x++)
+                Console.WriteLine("{0} = 0x{0:x}", x);
+        }
+    }
+}
