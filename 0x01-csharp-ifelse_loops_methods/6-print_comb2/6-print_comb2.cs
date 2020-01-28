01c0efec5dd1a50a8a05f360342a3a6181ae38d0
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-28 13:15:10 -0800

    prints all different combinations

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            for (; x < 10; x++)
+            {
+                int y = x + 1; 
+                for (; y < 10; y++)
+                {
+                    if (x == 8 && y == 9)
+                        Console.WriteLine("{0}{1}", x, y);
+                    else
+                        Console.Write("{0}{1}, ", x, y);
+                }
+            }
+        }
+    }
+}
