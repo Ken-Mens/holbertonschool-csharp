af2e329a3283a794f16c0024ddc63e2fea6fef64
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-28 13:08:26 -0800

    00 to 99 including spaces and commas

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            for (; x < 100; x++)
+            {
+                if (x != 99)
+                    Console.Write("{0:D2}, ", x);
+                else
+                    Console.Write("{0:D2}\n", x);
+            }
+        }
+    }
+}
