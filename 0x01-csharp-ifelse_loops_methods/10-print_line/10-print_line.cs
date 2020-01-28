9a4da0dd1402d90fbd3e78a79bedca51b7015cff
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-28 14:02:36 -0800

    method that draws a straight line in the terminal.

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int z = 0;
+        for (; z < length; z++)
+            Console.Write("_");
+        Console.WriteLine("");
+    }
+}
