5da66a7efbd2bd3b82e7f60148a1608a20f854dc
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-28 15:52:57 -0800

    print diagonal lines on terminal c sharp script

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int x = 0;
+        for (; x < length; x++)
+        {
+            int y = 0;
+            for (; y < x; y++)
+                Console.Write(" ");
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine("");
+    }
+}
