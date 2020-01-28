61fd455e656026a8d1b5cd38673074e7ea278853
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-28 12:07:18 -0800

    print alphabet in lowercase letters

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet_lower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char ltr = 'a'; ltr <='z'; ltr++)
+                Console.Write("{0}", ltr);
+        }
+    }
+}
