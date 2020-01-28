75aae4e410ac4ed823a04437b8d1a4904dc33bc7
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-28 12:32:34 -0800

    print alphabet excluding letter q and e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char ltr = 'a'; ltr <='z'; ltr++)
+              {
+                if (ltr != 'e' && ltr != 'q')
+                    Console.Write("{0}", ltr);
+              }
+        }
+    }
+}
