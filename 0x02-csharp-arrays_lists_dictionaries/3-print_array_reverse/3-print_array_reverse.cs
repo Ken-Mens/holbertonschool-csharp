13af62a073a8993b210393f651434d2824d9eef0
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 12:53:18 -0800

    print array in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array != null)
+         {
+             var deadend = array.Length - 1;
+             int idx = deadend;
+             for (; idx >= 0; idx --)
+            {
+                if (idx != deadend)
+                Console.Write(" ");
+                Console.Write(array[idx]);
+            }
+         }
+       Console.WriteLine("");
+    }
+}
