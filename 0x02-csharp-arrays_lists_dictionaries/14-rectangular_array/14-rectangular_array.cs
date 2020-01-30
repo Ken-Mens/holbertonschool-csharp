ae124533f05eb629a5ec46043dde5329fa8fbd6e
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 15:37:36 -0800

    creates and prints a 5 by 5 two-dimensional array and initialize index [2,2] to 1 and all other indices to 0.

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] gridbox = new int[5,5];
+        gridbox [2,2] = 1;
+        int x = 0;
+        for (; x < 5; x++)
+        {
+            int y = 0;
+            for (; y < 5; y++)
+            {
+                if (y != 0)
+                    Console.Write(" ");
+                 Console.Write(gridbox[x,y]);             
+            }
+            Console.WriteLine("");
+        }
+    }
+}
