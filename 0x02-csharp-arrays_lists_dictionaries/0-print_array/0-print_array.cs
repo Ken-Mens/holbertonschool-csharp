ff3037132444adbafe36017e09b22d72a61fafc9
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 12:15:27 -0800

    Print an array of integers

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] arr = new int[size];
+        for (int idx = 0; idx < size; idx++)
+        {
+            arr[idx] = idx;
+            if (idx != 0)
+                Console.Write(" ");
+            Console.Write(idx);
+        }
+        Console.WriteLine("");
+        return arr;
+    }
+}
