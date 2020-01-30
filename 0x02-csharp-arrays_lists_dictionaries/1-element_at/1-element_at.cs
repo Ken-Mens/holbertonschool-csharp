af3190791039e5805ab4faa761ed58ed2491a153
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 12:23:51 -0800

    method that retrieves an element from an array.

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        var deadend = array.Length - 1;
+        if (index < 0 || index > deadend)
+        {
+            Console.WriteLine("Index out of range");
+            return(-1);
+        }
+        return array[index];
+    }
+}
