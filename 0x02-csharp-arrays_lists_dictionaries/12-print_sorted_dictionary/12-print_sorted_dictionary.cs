8d7b96f3447a2a1a4ef0688cced49c8cb3f6994e
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 14:59:13 -0800

    print sorted dictionary in  csharp

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+          List<string> neoList = new List<string> ();
+          foreach (string keys in myDict.Keys)
+          neoList.Add(keys);
+          neoList.Sort();
+          foreach (string keys in neoList)
+            Console.WriteLine("{0}: {1}", keys, myDict[keys]);
+        }
+    }
