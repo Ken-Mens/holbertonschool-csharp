4a1c232bb0878162e4fa731b131144768c133e7e
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 13:54:10 -0800

    method that deletes the item at a specific position in a list.

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static void DeleteAt(List<int> myList, int index)
+        {
+            var deadend =  myList.Count - 1;
+            if (index < 0 || index > myList.Count - 1)
+                Console.WriteLine("Index is out of range");
+                else
+                myList.Remove(myList[index]);
+        }
+    }
+
