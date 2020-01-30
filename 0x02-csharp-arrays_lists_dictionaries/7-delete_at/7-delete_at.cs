e91b9ccf7d6be8e703c68584d891974535682896
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 15:59:23 -0800

    delete at list index

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
         public static void DeleteAt(List<int> myList, int index)
         {
             var deadend =  myList.Count - 1;
-            if (index < 0 || index > myList.Count - 1)
+            if (index < 0 || index > deadend)
                 Console.WriteLine("Index is out of range");
                 else
                 myList.Remove(myList[index]);
