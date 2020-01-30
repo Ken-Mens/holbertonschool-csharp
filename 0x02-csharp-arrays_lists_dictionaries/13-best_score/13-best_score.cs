b42e90e6bb975155bb7cceafebc09cc9b544fe0b
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 15:19:32 -0800

    method that returns the key with the biggest integer value in a given dictionary.

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+          List<int> neoList = new List<int> ();
+          Dictionary<int, string> neoDict = new Dictionary<int, string>();
+          foreach (KeyValuePair <string, int> mvp in myList)
+          {
+              neoDict[mvp.Value] = mvp.Key;
+              neoList.Add(mvp.Value);
+          }
+          if (neoList.Count == 0)
+                return ("None");
+                neoList.Sort();
+                var deadend = neoList.Count - 1;
+                return(neoDict[neoList[deadend]]);
+        }
+    }
