32cb40fd23114a0d37186335ac25abc6af469def
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 18:49:11 -0800

    fixed syntax of matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int mH = myMatrix.GetLength(0);
+        int mW = myMatrix.GetLength(1);
+        int temp = new int();
+        int[,] neoMatrix = new int[mH, mW];
+        int x = 0;
+        for (; x < mH; x++)
+        {
+            int y = 0;
+            for (; y < mW; y++)
+            {
+                temp = myMatrix[x, y];
+                neoMatrix[x, y] = temp * temp;
+            }
+        }
+        return (neoMatrix);
+    }
+}
