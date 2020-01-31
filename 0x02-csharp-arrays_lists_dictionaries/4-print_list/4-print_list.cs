1dd906dde99f03045d064bf79ac287868ef45e2b
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-30 18:34:37 -0800

    print list of integers

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+      if (size < 0)
+      {
+       Console.WriteLine("Size cannot be negative");
+       return null;
+      }
+    List <int> ilist = new List <int> ();
+    int x = 0;
+    for (; x < size; x++)
+    {
+        ilist.Add(x);
+        if (x != 0)
+
+        Console.Write(" ");
+        Console.Write(x);
+    }
+    Console.WriteLine(" ");
+    return ilist;
+    }
+}
