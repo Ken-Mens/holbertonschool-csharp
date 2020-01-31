95b962a53ce6009d6725bbc275d14e5d5538c102
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-01-31 10:29:05 -0800

    Write a program that creates and prints a jagged array containing 3 arrays.

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+  
+class GFG { 
+      
+    public static void Main() 
+    { 
+          
+        int[][] jagged_arr = new int[3][]; 
+  
+        jagged_arr[0] = new int[] {0, 1, 2, 3}; 
+        jagged_arr[1] = new int[] {0, 1, 2, 4, 5, 6}; 
+        jagged_arr[2] = new int[] {0, 1};  
+
+        int x = 0;
+        for (; x < jagged_arr.Length; x++) { 
+    
+            int y = 0;
+            for (; y < jagged_arr[x].Length; y++) { 
+                   if (y!= 0)
+                   Console.Write(" ");
+                   Console.Write(jagged_arr[x][y]); 
+            } 
+            Console.WriteLine(); 
+        } 
+    } 
+}
