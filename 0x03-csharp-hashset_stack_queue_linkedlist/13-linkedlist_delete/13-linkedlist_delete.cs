1de35a9ba9370448edb52fa3fbfbfc19e4dacdbf
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-10 14:40:14 -0800

    add delete node at given index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> newL = myLList.First;
+        int i = 0;
+        for (; i < index && newL.Next != null; i++)
+            newL = newL.Next;
+        if (i == index)
+            myLList.Remove(newL);
+    }
+}
