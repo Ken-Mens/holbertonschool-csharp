679d367d6fad9b53130d145d58a32d814f0048b0
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-10 13:49:30 -0800

    add linked list element

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newN = new LinkedListNode<int>(n);
+        myLList.AddFirst(newN);
+        return (newN);
+    }
+}
