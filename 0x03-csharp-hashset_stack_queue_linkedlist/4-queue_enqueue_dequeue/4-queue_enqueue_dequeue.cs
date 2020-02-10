72329f80d2b181757f951c2d2ba974879a691585
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-10 12:38:59 -0800

    add queue enque and deque task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+        class MyQueue
+
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        bool innit = aQueue.Contains(search);
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, innit);
+        if (innit == true)
+        {
+            while (aQueue.Dequeue() != search)
+                continue;
+        }
+        aQueue.Enqueue(newItem);
+        return (aQueue);
+    }
+}
+
