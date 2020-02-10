8be242a638808928de0a8911308c6d2630e0613f
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-10 11:49:19 -0800

    add common elements in two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+ {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+        List<int> nList = new List<int>();
+        foreach (int num in list1)
+        {
+            if (list2.Contains(num))
+                nList.Add(num);
+        }
+        nList.Sort();
+        return (nList);
+        }
+ }
