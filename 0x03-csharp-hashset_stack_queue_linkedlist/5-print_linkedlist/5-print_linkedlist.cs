be5779b7cf8bddea6ed8a6434375cf9673d2194a
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-10 12:47:12 -0800

    printing linked list with size count

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newlink = new LinkedList<int>();
+        for (int idx = 0; idx < size; idx++)
+        {
+            newlink.AddLast(idx);
+            Console.WriteLine(idx);
+        }
+        return (newlink);
+    }
+}
