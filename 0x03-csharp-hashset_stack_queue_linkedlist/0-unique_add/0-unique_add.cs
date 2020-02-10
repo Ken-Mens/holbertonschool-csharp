ee8caed5ecc1e064687a9d0c3ed15c079821fdc6
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-10 11:25:44 -0800

    unique add integers

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+           int sum = 0;
+           List <int> alist = myList;
+           alist.Sort();
+           int idx = 0;
+           for (; idx < alist.Count; idx++)
+            {
+                if (idx == 0 || alist[idx]!= alist[idx - 1])
+                    sum += alist[idx];
+            }
+            return (sum);
+        }
+    }
