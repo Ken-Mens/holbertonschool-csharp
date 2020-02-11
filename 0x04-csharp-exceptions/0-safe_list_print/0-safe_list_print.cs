a9d5c29449d90cb9f07b9483f0fc4fdf27a6d756
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-11 10:20:25 -0800

    add safe print list of numbers

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int idx = 0;
+        try
+        {
+            for (; idx < n; idx++)
+                Console.WriteLine(myList[idx]);
+        }
+        catch (Exception)
+        {
+            return (idx);
+        }
+        return (idx);        
+    }
+}
