2651a7f00ef292005755345979d6b0b3d5f7c982
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-11 14:05:12 -0800

    add constructor that takes parameters

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+enum Rating { Good, Great, Excellent };
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string nm, float ag, string own, Rating rt)
+    {
+        name = nm;
+        age = ag;
+        owner = own;
+        rating = rt;
+    }
+}
