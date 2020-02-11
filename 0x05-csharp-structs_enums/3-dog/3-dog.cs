3251773e1923b56f4f360f02fac970fb9595910d
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-11 14:11:57 -0800

    ovveride .ToString() method to print

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+enum Rating { Good, Great, Excellent };
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string nm, float ag, string own, Rating rt)
+    {
+        name = nm;
+        age = ag;
+        owner = own;
+        rating = rt;
+    }
+    public override string ToString()
+    {
+        return "Dog Name: " + name +
+                "\nAge: " + age +
+                "\nOwner: " + owner +
+                "\nRating: " + rating;
+    }
+}
