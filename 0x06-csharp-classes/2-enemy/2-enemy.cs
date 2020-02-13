5da8e1de8f9268926a3d3b2a8f502ed84b2dbf2c
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-13 10:53:38 -0800

    add pulbic constructor with new exception

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of zombie.</summary>
+        public int health;
+
+        ///<summary>Constructor made for Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+             if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+    }
+}
