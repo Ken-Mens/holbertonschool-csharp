a9af18a5efa3ec5118e1241c778ce2279e1dda28
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-13 11:41:44 -0800

    add e4-enemy

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of zombie.</summary>
+        private int health;
+
+        //<summary>Name field of zombie</summary>
+        private string name = "(No name)";
+
+        ///<summary>Constructor made for Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    ///<summary>Constructor with health value for Zombie</summary>
+        public Zombie (int value)
+        {
+             if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        ///<summary> Returns current health of zombie</summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+        ///<summary>Gets or sets the name.</summary>
+        public string Name
+        {
+            get {return (name);}
+            set {name = value;}
+        }
+    }
+}
