9f6e6a8f35c33d853b4aa51135c642ccfd6b36ea
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-13 11:06:29 -0800

    add Zombie health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of zombie.</summary>
+        private int health;
+
+        ///<summary>Constructor made for Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    ///<summary>Constructor with health value for Zombie</summary>
+        public Zombie(int value)
+        {
+             if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        ///<summary> Returns current health of zombie</summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
