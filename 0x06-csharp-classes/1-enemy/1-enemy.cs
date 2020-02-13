ac509ec41bd37f732d90aa9046fd4fe1dc72b076
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-13 10:35:50 -0800

    write public class Zombie that defines a zombie with public field health and public constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of zombie.</summary>
+        public int health;
+
+        ///<summary>Constructor made for Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
