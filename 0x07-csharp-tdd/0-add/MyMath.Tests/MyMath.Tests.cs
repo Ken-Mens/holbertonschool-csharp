07d0f4e229c888e48a4c6d35d5f073d82ed65acd
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-17 10:37:01 -0800

    add unit test and corresponding csproj file

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [Test]
+        public void Add_Positive()
+        {
+            int a = 66;
+            int b = 15;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Add_Negative_Positive()
+        {
+            int a = -9;
+            int b = 28;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Add_Negative()
+        {
+            int a = -25;
+            int b = -50;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+    }
+}
