bb4865d8484d0cebd1534efc1e77a288159e2b06
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-17 10:35:19 -0800

    MyMath for add method

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Performs math operations on inputted integers.</summary>
+    public class Operations
+    {
+        ///<summary>Function to add integerss.</summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
