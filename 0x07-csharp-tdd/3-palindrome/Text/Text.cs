0c9916facedc34b3185c6e3b9e6218f5f0a51a68
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-19 15:50:36 -0800

    add Text cs and csproj file

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Text
+{
+    ///<summary>operations on text string</summary>
+    public class Str
+    {
+        ///<summary>Method to determine if palindrome or not.</summary>
+        public static bool IsPalindrome(string s)
+        {
+            List<char> finder = new List<char>();
+            foreach (char letter in s)
+            {
+                if (!Char.IsPunctuation(letter) && !Char.IsWhiteSpace(letter))
+                {
+                    finder.Add(Char.ToLower(letter));
+                }
+            }
+            var ok = finder.Count;
+            if (ok == 0)
+                return (true);
+            List<char> reversal = new List<char>(finder);
+            reversal.Reverse();
+            return (finder.SequenceEqual(reversal));
+        }
+    }
+}
