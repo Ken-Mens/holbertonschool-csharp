fd650341c2cc3518e4245f4509be15247f43142e
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-19 15:51:10 -0800

    add unitest and csproj file

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsPalindrome_Punctuation()
+        {
+            string test = "!?. :;";
+            Assert.That(Text.Str.IsPalindrome(test), Is.True);
+        }
+        [Test]
+        public void IsPalindrome_A()
+        {
+            string test = "A man, a plan, a canal: Panama.";
+            Assert.That(Text.Str.IsPalindrome(test), Is.True);
+        }
+        [Test]
+        public void IsPalindrome_B()
+        {
+            string test = "Welcome! This is Holberton.";
+            Assert.That(Text.Str.IsPalindrome(test), Is.False);
+        }
+         [Test]
+        public void IsPalindrome_C()
+        {
+            string test = "Welcome! This is Holberton.";
+            Assert.That(Text.Str.IsPalindrome(test), Is.False);
+        }
+    }
+}
