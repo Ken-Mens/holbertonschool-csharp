be583cd72099bdb61c56eed0b480bacb651e4729
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 18:54:07 -0800

    add text method for counting string and csproj

---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Text
+{
+	///<summary>String operations</summary>
+	public class Str
+	{
+		///<summary>Method that returns  the number of words in said string</summary>
+		public static int CamelCase(string s)
+		{
+			if (s.Length == 0 || s == null)
+			{
+				return 0;
+			}
+			int count = 1;
+			foreach (char idx in s)
+			{
+				if (Char.IsUpper(idx))
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+	}
+}
