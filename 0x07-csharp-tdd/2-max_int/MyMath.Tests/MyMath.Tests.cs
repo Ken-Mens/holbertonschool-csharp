d92ec1e2bf0bf30ceb6ce508a1f9f096bf9a0dc7
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 16:30:31 -0800

    add test cases

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+	[TestFixture]
+	public class OperationsTests
+	{
+		[Test]
+		public void Max_test()
+		{
+			List<int> array = new List<int>();
+			array.Add(3);
+			array.Add(-4);
+			array.Add(9);
+			array.Add(5);
+			array.Add(1);
+
+			int output = Operations.Max(array);
+
+			Assert.AreEqual(9, output);
+		}
+
+		[Test]
+		public void Max_Empty_List()
+		{
+			List<int> array = new List<int>();
+
+			int output = Operations.Max(array);
+
+			Assert.AreEqual(0, output);
+		}
+	}
+}
