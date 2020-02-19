fbec947a8b3399bec59d98421117f4ee18980f52
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 16:06:48 -0800

    added test cases

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,49 +1,43 @@
-﻿using System;
-using System.IO;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Tests
 {
     [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-            StreamWriter standardOut =
-                new StreamWriter(Console.OpenStandardOutput());
-            standardOut.AutoFlush = true;
-            Console.SetOut(standardOut);
-        }
-        [Test]
-        public void Divide_Zero()
-        {
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
-                int[,] newMat = {
-                    {1, 2, 3},
-                    {4, 5, 6},
-                    {7, 8, 9}
-                };
-                int num = 0;
-                string output =
-                    string.Format("Num cannot be 0{0}", Environment.NewLine);
-                int[,] result = MyMath.Matrix.Divide(newMat, num);
-                Assert.That(output, Is.EqualTo(sw.ToString()));
-                Assert.That(result, Is.Null);
+      [Test]
+		public void Divide_M()
+		{
+			int[,] matrix = new int[2, 2] { { 8, 4 }, { 4, 10 } };
+			int num = 2;
 
-            }
-        }
-     
+			int[,] output = MyMath.Matrix.Divide(matrix, num);
+
+			Assert.AreEqual(new int[2, 2] { { 4, 2 }, { 2, 5 } }, output);
+		}
+	
+		[Test]
+		public void Divide_Zero()
+		{
+			int[,] matrix = new int[2, 2] { { 8, 2 }, { 2, 6 } };
+			int num = 0;
+
+			int[,] output = MyMath.Matrix.Divide(matrix, num);
+
+			Assert.AreEqual(null, output);
+		}
+
         [Test]
-        public void Divide_Empty()
-        {
-            int[,] newMat = {};
-            int num = 2;
-            int [,] output = {};
-            int[,] result = MyMath.Matrix.Divide(newMat, num);
-            Assert.That(result, Is.EqualTo(output));
-        }
+		public void Divide_NULL()
+		{
+			int[,] matrix = new int[2, 2] { { 8, 4 }, { 2, 6 } };
+			int nope = 0;
+
+			int[,] output = MyMath.Matrix.Divide(matrix, nope);
+
+			Assert.AreEqual(null, output);
+		}
+
+
     }
 }
