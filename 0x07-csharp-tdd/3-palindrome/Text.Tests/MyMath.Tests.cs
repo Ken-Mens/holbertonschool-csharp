be36a85398ce3187fa71e10dc4215c3d8f6f82a8
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 18:35:51 -0800

    add test plus csproj

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/MyMath.Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Text.Tests
+{
+	[TestFixture]
+	public class StrTests
+	{
+		[Test]
+		public void OK_Palindrome()
+		{
+			string s = "Racecar";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsTrue(output);
+		}
+
+		[Test]
+		public void Check_Palindrome()
+		{
+			string s = "A man, a plan, a canal: Panama.";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsTrue(output);
+		}
+
+		[Test]
+		public void Empty_String()
+		{
+			string s = "";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsTrue(output);
+		}
+	}
+}
