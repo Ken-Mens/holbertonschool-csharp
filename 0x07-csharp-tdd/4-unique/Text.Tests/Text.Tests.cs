60553ad6a6be016f744ea21e50067b2df830eaf4
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 20:01:56 -0800

    add text tests and csproj

---
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace Text.Tests
+{
+	///<summary>Unit testing for unique string</summary>
+	[TestFixture]
+	public class StrTexts
+	{
+		///<summary>checking for normal operation</summary>
+		[Test]
+		public void Unique_check()
+		{
+			string st = "ssissss";
+
+			int output = Str.UniqueChar(st);
+
+			Assert.AreEqual(2, output);
+		}
+
+
+		///<summary>check if string is empty</summary>
+		[Test]
+		public void Empty_check()
+		{
+			string st = "";
+
+			int output = Str.UniqueChar(st);
+
+			Assert.AreEqual(-1, output);
+		}
+
+	}
+}
