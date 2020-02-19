e8df02c4cc7e3982ba499292cb818843b0655a01
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 18:54:39 -0800

    add test cases

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CamelC_Empty()
+        {
+            string test = "";
+            int result = Text.Str.CamelCase(test);
+            Assert.That(result, Is.EqualTo(0));
+        }
+        [Test]
+        public void CamelC_One()
+        {
+            string test = "wee";
+            int result = Text.Str.CamelCase(test);
+            Assert.That(result, Is.EqualTo(1));
+        }
+        [Test]
+        public void CamelC_Null()
+        {
+            string test = null;
+            int result = Text.Str.CamelCase(test);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
