e0f61217952efef1e5173285af567cef05f627d9
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 16:31:35 -0800

    add method to find largest in list

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+	///<summary> Operations class</summary>
+	public class Operations
+	{
+		///<summary>Method to find the largest in the list of nums</summary>
+		public static int Max(List<int> nums)
+		{
+           int num = 0;
+           var ok = nums.Count;
+            if (ok != 0)
+            {
+                nums.Sort();
+                num = nums[nums.Count - 1];
+            }
+            return (num);
+		}
+	}
+}
