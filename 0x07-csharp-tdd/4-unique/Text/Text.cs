07e622792b4c3b8fdd17e7fc5cd1b244384b5f67
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 19:42:50 -0800

    method to find unique index

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Text
+{
+	///<summary> string operations</summary>
+	public class Str
+	{
+		///<summary> method to get first unique index</summary>
+		public static int UniqueChar(string s)
+		{
+			if (s == "")
+			{
+				return (-1);
+			}
+
+			char bc = s[0];
+            var sizing =  s.Length;
+            int index = 0;
+			for (; index < sizing; index++)
+			{
+				if (s[index] != bc)
+				{
+					return index;
+				}
+				bc = s[index];
+			}
+			return (-1);
+		}
+	}
+}
