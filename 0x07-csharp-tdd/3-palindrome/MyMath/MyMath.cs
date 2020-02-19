6a8e4d5287f647f6fd070bebba76e6027309a54a
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-18 18:19:25 -0800

    add Mymath method for palindrome and csproj

---
+++ 0x07-csharp-tdd/3-palindrome/MyMath/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Text
+{
+    class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            int length = s.Length;
+            for (int idx = 0; idx < length / 2; idx++)
+            {
+                if (s[idx] != s[length - idx - 1])
+                return false;
+            }
+            return true;
+        }
+    }
+}
