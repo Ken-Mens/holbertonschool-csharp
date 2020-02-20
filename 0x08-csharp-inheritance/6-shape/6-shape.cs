df5e873ba049f298c4eafc06cb0ad6e169f39e69
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-20 11:50:58 -0800

    class Rectangle that inherits from Shape

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+///<summary>Class for Shape</summary>
+class Shape
+{
+	///<summary>Throw not implemented error</summary>
+	public virtual int Area()
+	{
+		throw new System.NotImplementedException("Area() is not implemented");
+	}
+}
+
+///<summary>Rectangle Class inheriting from Shape</summary>
+class Rectangle : Shape
+{
+	private int width;
+	private int height;
+
+	//Rectangle width
+	public int Width
+	{
+		get
+		{
+			return width;
+		}
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Width must be greater than or equal to 0");
+			}
+			width = value;
+		}
+	}
+
+	//Rectangle height
+	public int Height
+	{
+		get
+		{
+			return height;
+		}
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Height must be greater than or equal to 0");
+			}
+			height = value;
+		}
+	}
+}
