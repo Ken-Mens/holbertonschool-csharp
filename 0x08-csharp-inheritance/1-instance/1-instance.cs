d4656ab75389408d42db31fa44d04afee6ec3945
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-20 10:31:15 -0800

    method to check instance

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    /// <summary> Object class</summary>
+    class Obj
+    {
+
+    /// <summary> method that returns True if the object is an instance of</summary>
+  
+
+        public static bool IsInstanceOfArray(object obj)
+        {
+            return typeof(Array).IsInstanceOfType(obj);
+        }
+    }
