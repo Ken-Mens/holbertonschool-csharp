7167b271d7fcbc6d956c53dd2d6ceef12bdf4b42
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-20 10:43:11 -0800

    check if subclass or not

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    /// <summary> Object class</summary>
+    class Obj
+    {
+
+    /// <summary> method that  checks if subclass or not</summary>
+  
+
+        public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+        {
+            return derivedType.IsSubclassOf(baseType);
+        }
+    }
