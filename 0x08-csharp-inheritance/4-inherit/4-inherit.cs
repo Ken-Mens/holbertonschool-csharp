754ca7552bcf876a68d1503800b42ff686d20a9a
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-20 11:28:16 -0800

    add inherit animal class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Base class</summary>
+class Animal
+{
+}
+
+///<summary>Dog Class that inherits from Animal</summary>
+class Dog : Animal
+{
+}
