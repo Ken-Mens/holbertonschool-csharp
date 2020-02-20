62c411b4ac6a22eb928842f461b10823557cd077
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-20 11:25:04 -0800

    add get type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+    /// <summary> Object class</summary>
+    class Obj
+    {
+
+    /// <summary> prints the names of the available properties</summary>
+  
+
+       ///<summary>Printout all propoerties and methods of object</summary>
+    public static void Print(object myObj)
+	{
+		TypeInfo idx = myObj.GetType().GetTypeInfo();
+		Console.WriteLine("{0} Properties:", idx.Name);
+		foreach (PropertyInfo prop in idx.GetProperties())
+		{
+			Console.WriteLine(prop.Name);
+		}
+
+		Console.WriteLine("{0} Methods:", idx.Name);
+		foreach (MethodInfo metInfo in idx.GetMethods())
+		{
+			Console.WriteLine(metInfo.Name);
+		}
+	}
+    }
