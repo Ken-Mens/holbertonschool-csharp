d36c7aaabc5d576dd00b81eaac501d872ef16917
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-20 10:16:57 -0800

    add method that returns if type int or not

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    /// <summary> Object class</summary>
+    class Obj
+    {
+
+    /// <summary> method that returns true if obj is int</summary>
+  
+
+        public static bool IsOfTypeInt(object obj)
+        {
+            return obj is int;
+        }
+    }
