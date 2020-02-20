bd279a260880e0df7dc982f710c3a40014b246fb
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-20 11:41:53 -0800

    class shape with thrown exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Class for Shape</summary>
+class Shape
+{
+	///<summary>Throws an error</summary>
+	public virtual int Area()
+	{
+		throw new System.NotImplementedException("Area() is not implemented");
+	}
+}
