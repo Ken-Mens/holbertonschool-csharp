ca890341a4c53f068f60162c88c30432739e5402
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-11 16:25:08 -0700

    add vector addition method

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Algebraic operations on vectors.</summary>
+class VectorMath
+{
+    ///<summary>Adds 2d and 3d vectors alltogether.</summary>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] triple = new double[1] {-1};
+
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            double[] eureka = { 0, 0 };
+			eureka[0] = vector1[0] + vector2[0];
+			eureka[1] = vector1[1] + vector2[1];
+			return eureka;
+        }
+        else if (vector1.Length == 3 && vector1.Length == vector2.Length)
+        {
+            triple = new double[3] {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2]+vector2[2]};
+        }
+        return (triple);
+    }
+}
