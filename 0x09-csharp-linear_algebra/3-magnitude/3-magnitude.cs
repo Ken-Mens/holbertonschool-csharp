e157d6f8ee20fe19df8e9fe60c473fb20c4a9da6
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-11 12:21:11 -0700

    method that calculates magnitude of 2d and 3d vectors

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Class for operations on vectors.</summary>
+class VectorMath
+{
+    ///<summary>Calculates magnitude from component vectors.</summary>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+        {
+    		double eureka = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2));
+			return Math.Round(eureka, 2);
+        }
+        if (vector.Length == 3)
+        {
+            double eureka = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2));
+			return Math.Round(eureka, 2);
+        }
+        return (-1);
+    }
+}
