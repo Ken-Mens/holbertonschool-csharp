0ecb2e7d4e2dab94f0876b3f60f0ef2898151711
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-11 20:41:40 -0700

    add dot product 2d and 3d vector

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace _12_dot_product
+///<summary>Class for vector math</summary>
+class VectorMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	///<summary>calculating vector dot product for 2d and 3d</summary>
+	public static double DotProduct(double[] vector1, double[] vector2)
+	{
+        double eureka = -1;
+
+		if (vector1.Length == 2 && vector2.Length == 2)
+		{
+			return vector1[0] * vector2[0] + vector1[1] * vector2[1];
+		}
+		else if (vector1.Length == 3 && vector2.Length == 3)
+		{
+			return vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2];
+		}
+		return (eureka);
+	}
 }
