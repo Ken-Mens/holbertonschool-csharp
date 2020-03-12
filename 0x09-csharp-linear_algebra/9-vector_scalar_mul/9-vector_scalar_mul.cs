24c42fa647066b408958d662fc83c7554a7fd922
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-11 20:50:45 -0700

    add vector scalar

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+///<summary>Class for vector math</summary>
+class VectorMath
+{
+	///<summary>Class Operation that calculates  vector scaling</summary>
+	///<return>the resulting vector</return>
+	public static double[] Multiply(double[] vector, double scalar)
+	{
+        double[] vector3 = new double[1] {-1};
+
+		if (vector.Length == 2)
+		{
+			double[] result = { 0, 0 };
+			result[0] = vector[0] * scalar;
+			result[1] = vector[1] * scalar;
+			return result;
+		}
+		else if (vector.Length == 3)
+		{
+		    vector3 = new double[3] {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};
+		}
+		return (vector3);
+	}
+}
