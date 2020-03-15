85ea8a107869d3ca48d535aee9e18065319cbf58
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-14 18:55:00 -0700

    transpose matrix and return resulting matrix

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MatrixMath
+{
+	/// <summary>  method to transpose a matrix and return the resulting matrix </summary>
+	/// <param name="matrix">The matrix to be transposed</param>
+	/// <returns> transposed matrix.</returns>
+	public static double[,] Transpose(double[,] matrix)
+	{
+        int Wget = matrix.GetLength(1);
+        int Hget = matrix.GetLength(0);
+		double[,] newMatrix = new double[Wget, Hget];
+        int x = 0;
+		for (; x < Wget; x++)
+		{
+            int y = 0;
+			for (; y < Hget; y++)
+			{
+				newMatrix[x, y] = matrix[y, x];
+			}
+		}
+		return (newMatrix);
+	}
+}
