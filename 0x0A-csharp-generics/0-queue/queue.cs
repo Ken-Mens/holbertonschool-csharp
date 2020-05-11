417d1b7c14c9dcef96a1d52073ab41fa4ad363ba
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-05-11 12:06:00 -0700

    add task 0 Queue folder

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+/// <summary>Class that stores a queue of objects of same type.</summary>
+class Queue<T>
+{
+    /// <summary>Returns type stored.</summary>
+    /// <returns>Type of the T parameter.</returns>
+    public Type CheckType() {
+        return typeof(T);
+    }
+}
