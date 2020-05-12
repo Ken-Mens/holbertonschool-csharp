d40f4ee09f0c6a7a4bae9c7d2b55992069a87efb
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-05-12 14:04:20 -0700

    add user interface task

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+///<summary>Base class.</summary>
+public abstract class Base
+{
+    ///<summary>Name of instance.</summary>
+    public string name;
+
+    ///<summary>String override.</summary>
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType().ToString());
+    }
+}
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+    public void Interact()
+    {
+    }
+    public void Break()
+    {
+    }
+    public void Collect()
+    {
+    }
+}
