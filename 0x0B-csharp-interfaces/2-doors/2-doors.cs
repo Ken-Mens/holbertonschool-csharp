c1d26e3f8bdb545189c0b434ed6f8cb468db0257
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-05-12 14:25:09 -0700

    add 2 doors interface task

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+///<summary>Base class.</summary>
+public abstract class Base
+{
+    ///<summary>Name of instance.</summary>
+    public string name;
+
+    ///<summary>String override.</summary>
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType().ToString());
+    }
+}
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+class Door : Base, IInteractive
+{
+
+    public Door(string val = "Door")
+    {
+        name = val;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
