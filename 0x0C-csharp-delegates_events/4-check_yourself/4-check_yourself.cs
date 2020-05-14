c786599f518aca689d4a88de388c402994d24ae8
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-05-13 17:40:25 -0700

    add Checkstatus class

---
+++ 0x0C-csharp-delegates_events/4-check_yourself/4-check_yourself.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+///<summary>Calculates modifiers.</summary>
+public enum Modifier
+{
+    ///<summary>Weak modifier</summary>
+    Weak = 1,
+    ///<summary>Base modifier</summary>
+    Base = 2,
+    ///<summary>Strongest modifier</summary>
+    Strong = 3
+}
+
+/// <summary>Will apply effect based on tier modifier.</summary>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+/// <summary>Will calculate health.</summary>
+
+public delegate void CalculateHealth(float health);
+
+class CurrentHPArgs : EventArgs
+{
+    public readonly float currentHp;
+
+    public CurrentHPArgs(float newHp)
+    {
+        this.currentHp = newHp;
+    }
+}
+
+///<summary> Public Player class</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+    private string status;
+
+    ///<summary>Event handlet for HP of player</summary>
+    EventHandler<CurrentHPArgs> HPCheck;
+
+    ///<summary>Player Class Constructor.</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+        this.status = $"{this.name} is ready to go!";
+        HPCheck += CheckStatus;
+    }
+
+    ///<summary>Prints Player health.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+       ///<summary> Will calculate Player damage.</summary>
+    public void TakeDamage(float damage)
+      {
+        if (damage < 0f)
+            damage = 0f;
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+        float newHp = this.hp - damage;
+        this.ValidateHP(newHp);
+      }
+    ///<summary>Will calculate Player healing.</summary>
+    public void HealDamage(float heal)
+    {
+        Console.WriteLine($"{this.name} heals {heal} HP!");
+           if (this.hp >= 0)
+            this.ValidateHP(this.hp + heal);
+    }
+
+    ///<summary>Sets new value of Player hp.</summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+            this.hp = 0;
+        else if (newHp > this.maxHp)
+            this.hp = this.maxHp;
+        else 
+            this.hp = newHp;
+        HPCheck(this, new CurrentHPArgs(this.hp));
+    }
+    ///<summary>Will Validate Player hp modifications.</summary>
+      public float ApplyModifier(float baseValue, Modifier modifier) 
+      {
+       return (baseValue * ((float)modifier / 2f));
+      }
+    ///<summary>Checks status of player.</summary>
+        private void CheckStatus(object sender, CurrentHPArgs ok)
+    {
+        if (ok.currentHp == this.maxHp)
+            this.status = $"{this.name} is in perfect health!";
+        else if (ok.currentHp >= (this.maxHp / 2))
+            this.status = $"{this.name} is doing well!";
+        else if (ok.currentHp >= (this.maxHp / 4))
+            this.status = $"{this.name} isn't doing too great...";
+        else if (ok.currentHp > 0)
+            this.status = $"{this.name} needs help!";
+        else
+            this.status = $"{this.name} is knocked out!";
+        Console.WriteLine(this.status);
+    }
+}
